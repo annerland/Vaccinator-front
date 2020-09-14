9d9d79eb2bdb75af7f5a74739f1c6b84904f2a86
Anna Campelo <annaacampelo@gmail.com>
2020-09-13 22:23:30 -0300

    created establishments route and dropDown component

---
+++ src/components/molecules/dropDown/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+import './index.scss'
+
+const DropDown = (props) => {
+  const [show, setShow] = useState(false)
+
+  const showInfo = () => {
+    show ? setShow(false) : setShow(true)
+  }
+
+  return (
+    <div className='drop-down'>
+      <div onClick={props.onClick} className='flex-text-name'>
+        <i className='icon-hospital' />
+        <p className='field'>Unidade:</p>
+        <p className='field-name'>{props.name}</p>
+      </div>
+
+      <div onClick={props.onClick} className='flex-text'>
+        <p className='field'>Endereço:</p>
+        <p>{props.adress}</p>
+      </div>
+
+      <i onClick={() => showInfo()} className={show ? 'icon-arrow-up' : 'icon-arrow-down'} />
+
+      {show &&
+        <div className='info'>
+          <p className='title'>Informações adicionais</p>
+
+          <div className='flex-text'>
+            <p className='field'>Vacinas disponíveis:</p>
+            <p>{props.vaccine}</p>
+          </div>
+
+          <div className='flex-text'>
+            <p className='field'>Bairro:</p>
+            <p>{props.district}</p>
+          </div>
+
+          <div className='flex-text'>
+            <p className='field'>CEP:</p>
+            <p>{props.cep}</p>
+          </div>
+
+          <p className='information'>Informe aos usuários que há outras vacinas disponíveis <span>aqui</span></p>
+        </div>}
+    </div>
+  )
+}
+
+DropDown.propTypes = {
+  name: PropTypes.string,
+  onClick: PropTypes.func,
+  adress: PropTypes.string,
+  vaccine: PropTypes.string,
+  district: PropTypes.string,
+  cep: PropTypes.string
+}
+
+export default DropDown
--- src/routes/user/establishments/index.js
+++ src/routes/user/establishments/index.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import Select from 'Components/atoms/select'
+import Search from 'Components/atoms/search'
+import DropDown from 'Components/molecules/dropDown'
+import React, { useState } from 'react'
+import Api from 'Util/api'
+
+import './index.scss'
 
 export default function EstablishmentsUser () {
+  const [establishments, setEstablishments] = useState([])
+
+  const fetchEstablishments = async () => {
+    console.log('test')
+  }
+
   return (
-    <h1>Estabelecimentos</h1>
+    <div className='establishments-route'>
+      <h1 className='title'>Estabelecimentos</h1>
+
+      <div className='establishments-header'>
+        <div className='search-container'>
+          <Search placeholder='Pesquisar estabelecimento' />
+        </div>
+        <div className='search-container'>
+          <Select placeholder='Filtrar por Distância' />
+        </div>
+        <div className='search-container'>
+          <Select placeholder='Filtrar por Vacina' />
+        </div>
+        <p>Limpar filtros</p>
+      </div>
+
+      <DropDown />
+    </div>
   )
 }
